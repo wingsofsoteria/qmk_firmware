b934087d2d1162a6d312bd1bc1929e7f7673aef0
Soteria <voidmeridian@gmail.com>
2025-03-21 17:12:48 -0400

    UPDATE KEYMAP

--- keyboards/keychron/q5_max/ansi_encoder/config.h
+++ keyboards/keychron/q5_max/ansi_encoder/config.h
@@ -43,7 +43,7 @@
 #    define RGB_MATRIX_BRIGHTNESS_TURN_OFF_VAL 32
 
 /* Indications */
-#    define NUM_LOCK_INDEX 32
+#    define NUM_LOCK_INDEX 14
 #    define CAPS_LOCK_INDEX 55
 #    define LOW_BAT_IND_INDEX \
         { 92 }
--- keyboards/keychron/q5_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/q5_max/ansi_encoder/keymaps/via/keymap.c
@@ -25,8 +25,8 @@ enum layers {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [WIN_BASE] = LAYOUT_ansi_101(
-        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   _______,  _______,  _______,    KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,    KC_PMNS,
+        KC_ESC,             KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,   KC_F12,             KC_DEL,   KC_NUM,   KC_SLEP,  _______,    KC_MUTE,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,    KC_EQL,   KC_BSPC,            KC_PGUP,  KC_EQL,   KC_PSLS,  KC_PAST,    KC_PMNS,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,    KC_RBRC,  KC_BSLS,            KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,             KC_HOME,  KC_P4,    KC_P5,    KC_P6,      KC_PPLS,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,  KC_UP,              KC_P1,    KC_P2,    KC_P3,
@@ -45,8 +45,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,            KC_RCTL,   KC_LEFT,  KC_DOWN,  KC_RIGHT,  _______,
         _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,              _______,  _______,             _______,  _______,  _______,
         _______,  _______,  _______,                                _______,                                _______,  MO(WIN_FN), _______,  _______,  _______,  _______,             _______,  _______,   _______),
-    
-        
+
+
 };
 
 // clang-format on
@@ -67,12 +67,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
-        rgb_matrix_set_color(89, RGB_RED);
-        rgb_matrix_set_color(72, RGB_RED);
-        rgb_matrix_set_color(55, RGB_RED);
-        rgb_matrix_set_color(36, RGB_RED);
-        rgb_matrix_set_color(17, RGB_RED);
-        rgb_matrix_set_color(0, RGB_RED);
+        rgb_matrix_set_color(89, RGB_PURPLE);
+        rgb_matrix_set_color(72, RGB_PURPLE);
+        rgb_matrix_set_color(55, RGB_PURPLE);
+        rgb_matrix_set_color(36, RGB_PURPLE);
+        rgb_matrix_set_color(17, RGB_PURPLE);
+        rgb_matrix_set_color(0, RGB_PURPLE);
     }
     if (get_highest_layer(layer_state) > 0) {
         uint8_t layer = get_highest_layer(layer_state);
@@ -83,7 +83,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
                 if (index >= led_min && index < led_max && index != NO_LED &&
                 keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_RED);
+                    rgb_matrix_set_color(index, RGB_PURPLE);
                 }
             }
         }
