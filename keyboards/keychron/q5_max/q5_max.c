f0449750a1c939d850ba99e2bd692e98cba1302e
Soteria <voidmeridian@gmail.com>
2025-03-21 16:23:18 -0400

    add custom keymap

--- keyboards/keychron/q5_max/q5_max.c
+++ keyboards/keychron/q5_max/q5_max.c
@@ -28,16 +28,7 @@
 #endif
 
 
-#ifdef DIP_SWITCH_ENABLE
-bool dip_switch_update_kb(uint8_t index, bool active) {
-    if (index == 0) {
-        default_layer_set(1UL << (active ? 2 : 0));
-    }
-    dip_switch_update_user(index, active);
 
-    return true;
-}
-#endif
 
 void keyboard_post_init_kb(void) {
 #ifdef LK_WIRELESS_ENABLE
